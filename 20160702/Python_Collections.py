efb3e52a7a314cd24fbcad6b4a4f61369a5ba004
Vuyisile <vuyisilemark@gmail.com>
2016-07-02 03:27:09 -0400

    deleted and added items onmy list

--- 20160702/Python_Collections.py
+++ 20160702/Python_Collections.py
@@ -1,3 +1,8 @@
 my_sisonke_rising_crew = ['Johannes','Florah','Collen','Emmanuel','Vuyisile','Tefo','Wiseman']
 print(my_sisonke_rising_crew[0])
 
+my_sisonke_rising_crew.append('Xolani')
+print (my_sisonke_rising_crew)
+
+del my_sisonke_rising_crew[2], my_sisonke_rising_crew[0]
+print(my_sisonke_rising_crew)
