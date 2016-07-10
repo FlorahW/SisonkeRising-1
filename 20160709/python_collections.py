df086a525a7d882411ef086cab8252b89e5ec8e5
Vuyisile <vuyisilemark@gmail.com>
2016-07-10 04:26:42 -0400

    created a display funtions that displays my dictionary list

--- 20160709/python_collections.py
+++ 20160709/python_collections.py
@@ -1,8 +1,26 @@
-my_cars = ["Honda","Ford","Jeep","Chervolet SS"]
+'''my_cars = ["Honda","Ford","Jeep","Chervolet SS"]
+
 
 my_cars.append("BMW")
 
 del my_cars[0]
 
-for car in my_cars:
-    print car
+for car in range(0,4):
+    print (car,my_cars[car])'''
+    
+
+my_cars = {1 : 'Honda', 2 : 'Ford', 3 : 'Jeep', 4 : 'Chervolet SS', 5 : 'BMW'}
+
+def display(my_cars):
+
+    return(my_cars)
+#for cars in my_cars:
+    #print(cars ,my_cars[cars]) #THE CODE EXECUTES THE KEY AND VALUE.
+    
+
+display(my_cars)  
+
+
+
+
+
